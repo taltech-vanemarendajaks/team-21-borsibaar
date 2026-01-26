42c4611057f2b1198ea4d094bdf5ed344dcf0e99
Kevindaman <kevinpoljans@hotmail.com>
2026-01-26 15:29:36 +0200

    fix: updated validation of OrganizationRequestDto

--- backend/src/main/java/com/borsibaar/dto/OrganizationRequestDto.java
+++ backend/src/main/java/com/borsibaar/dto/OrganizationRequestDto.java
@@ -4,8 +4,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.DecimalMin;
 import java.math.BigDecimal;
 
+import io.micrometer.common.lang.NonNull;
+
 public record OrganizationRequestDto(
-        @NotBlank String name,
-        @DecimalMin("0.00") BigDecimal priceIncreaseStep,
-        @DecimalMin("0.00") BigDecimal priceDecreaseStep) {
+        @NotBlank @Size(max = 100, message="Name must be at most 100 characters") String name,
+        @NonNull(message="Price increase step is required") @DecimalMin("0.0") BigDecimal priceIncreaseStep,
+        @NonNull(message="Price decrease step is required") @DecimalMin("0.0") BigDecimal priceDecreaseStep) {
 }
